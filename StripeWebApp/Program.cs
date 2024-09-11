a9d96dbdfe25aded0b994d3322511036d4573b2a
biplavpoudel <biplavpoudel764@gmail.com>
2024-09-11 19:48:13 +0545

    Add project files.

---
+++ StripeWebApp/Program.cs
@@ -0,0 +1,38 @@
+namespace StripeWebApp;
+using Stripe;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<String>();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=PaymentController}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
